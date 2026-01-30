aa17bca035dd6ba61e7db23843eef9ab9a2bf576
Kevin5621 <yohanes123kevin123@gmail.com>
2026-01-30 16:01:29 +0700

    feat: Refactor portfolio layout by integrating contact information into HowIWork and removing dedicated Experience and Footer components.

--- src/components/portfolio/PortfolioHome.tsx
+++ src/components/portfolio/PortfolioHome.tsx
@@ -1,12 +1,9 @@
-import React, { useLayoutEffect, useRef } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 import Lenis from 'lenis';
 import { Hero } from './Hero';
 import { Story } from './Story';
-import { Experience } from './Experience';
 import { Projects } from './Projects';
 import { HowIWork } from './HowIWork';
-import { Footer } from './Footer';
-
 import 'lenis/dist/lenis.css';
 
 export const PortfolioHome = () => {
@@ -40,10 +37,8 @@ export const PortfolioHome = () => {
     <div className="portfolio-wrapper min-h-screen">
       <Hero />
       <Story />
-      <Experience />
       <Projects />
       <HowIWork />
-      <Footer />
     </div>
   );
 };
